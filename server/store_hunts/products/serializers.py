88132daa897392bc9a5ed0097451c493602b4169
Null <kelanidarasimi9@gmail.com>
2024-04-27 14:09:23 +0100

    requirement file updated

---
+++ server/store_hunts/products/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from models import *
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    pass
+class CategorySerializer(serializers.ModelSerializer):
+    pass
+class ProductCreateSerializer(serializers.ModelSerializer):
+    pass
