1fa6daf8d1059f5b7236baeb1370cbef68f17f35
Null <kelanidarasimi9@gmail.com>
2024-05-22 23:26:41 +0100

    add default authentication to drf settings

--- server/store_hunts/store_hunts/settings.py
+++ server/store_hunts/store_hunts/settings.py
@@ -196,6 +196,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
